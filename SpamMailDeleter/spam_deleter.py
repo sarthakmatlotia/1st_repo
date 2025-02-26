4dd0bcd4b94a9c87489c98e5cb470bf918429f8d
Sarthak Matlotia <75120201+sarthakmatlotia@users.noreply.github.com>
2025-02-26 23:53:41 +0530

    Update spam_deleter.py

--- SpamMailDeleter/spam_deleter.py
+++ SpamMailDeleter/spam_deleter.py
@@ -85,7 +85,7 @@ def delete_spam_emails():
             print(f"⚠️ Unexpected error: {e}")
 
         print("⏳ Waiting 30 minutes before next check...\n")
-        time.sleep(300)
+        time.sleep(1800)
 
 # Start the services
 keep_alive()
